dbaa9075a332ffb76aaf0a884ceb5073a6ebbd96
amanchopra1 <98337430+amanchopra1@users.noreply.github.com>
2023-01-06 01:07:08 +0530

    initial commit

---
+++ home/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, HttpResponse,redirect
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
+
+
+
+
+
+# Create your views here.
+def index(request):
+    # context ={
+    #     "variable1":"Harry is great"
+    # }
+    return render(request, 'index.html')
+    # return HttpResponse("this is homepage")
+
+def about(request):
+    return render(request, 'about.html')
+   
+
+def services(request):
+    return render(request, 'services.html')
+   
+
+def contact(request):
+    if request.method=="POST":
+        name= request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name, email=email, phone=phone, desc=desc, date= datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent')
+       
+
+    return render(request, 'contact.html')
+
+def courses(request):
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect("/login") 
+    return render(request, 'course.html')
+
+def loginUser(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+       # check if user has entered correct credentials
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+        # A backend authenticated the credentials
+            login(request,user)
+            return redirect("/course")
+
+        else:
+            # No backend authenticated the credentials
+            return render(request,'login.html')
+
+    return render(request,'login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
+
+    
+
+
+
+
+
+
